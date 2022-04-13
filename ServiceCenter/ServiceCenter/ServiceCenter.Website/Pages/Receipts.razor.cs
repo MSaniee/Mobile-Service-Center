6405797f94fc9e9479ccc1e0e71bc7ca3bb8bbfb
Mohsen Saniee <Saniee2@outlook.com>
2022-04-13 12:25:02 +0430

    Add Pagination Compenent to My Receipt Page

--- ServiceCenter/ServiceCenter/ServiceCenter.Website/Pages/Receipts.razor.cs
+++ ServiceCenter/ServiceCenter/ServiceCenter.Website/Pages/Receipts.razor.cs
@@ -9,15 +9,24 @@ public partial class Receipts
 {
     public List<ReceiptDto> ReceiptList { get; set; } = new();
     public MetaData MetaData { get; set; } = new MetaData();
-    private Pagable Pagable = new() { Page = 1, PageSize = 10 , Search = null};
+    private Pagable Pagable = new() { Page = 1, PageSize = 5 , Search = null};
 
     [Inject]
     public IReceiptService ReceiptService { get; set; }
 
+
     protected async override Task OnInitializedAsync()
     {
-        var pagingResponse  = await ReceiptService.GetReceipts(Pagable);
-
+        await GetReceipts();
+    }
+    private async Task SelectedPage(int page)
+    {
+        Pagable.Page = page;
+        await GetReceipts();
+    }
+    private async Task GetReceipts()
+    {
+        var pagingResponse = await ReceiptService.GetReceipts(Pagable);
         ReceiptList = pagingResponse.Items;
         MetaData = pagingResponse.MetaData;
     }
