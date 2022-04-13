c990216181b5622cef7ed68628a5df33c1b27ff8
Mohsen Saniee <Saniee2@outlook.com>
2022-04-13 09:51:11 +0430

    Add My Receipts page

---
+++ ServiceCenter/ServiceCenter/ServiceCenter.Website/Components/Receipts/ReceiptTable.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using ServiceCenter.Application.Dtos.Receipts;
+
+namespace ServiceCenter.Website.Components.Receipts;
+
+public partial class ReceiptTable
+{
+    [Parameter]
+    public List<ReceiptDto> Receipts { get; set; }
+}
+
